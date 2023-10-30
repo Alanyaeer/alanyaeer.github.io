5533cdc0971719834883fdde48c3f7f76ce44f11
Alan <1563085328@qq.com>
2023-10-30 19:55:53 +0800

    Site updated: 2023-10-30 19:55:53

--- js/foot.js
+++ js/foot.js
@@ -1,15 +1,15 @@
-// 动态心跳，更改自己的名称
+// 动态心跳，更改自己的名�?
 $(document).ready(function(e){
-    $('.copyright').html('©2022 <i class="fa-fw fas fa-heartbeat card-announcement-animation cc_pointer"></i> By Hao的空间');
+    $('.copyright').html('©2022 <i class="fa-fw fas fa-heartbeat card-announcement-animation cc_pointer"></i> By Hao的空�?);
 })
-​
+�?
 $(document).ready(function(e){
     show_date_time();
 })
-​
+�?
 //本站运行时间，更改自己建立站点的时间
 function show_date_time(){
-$('.framework-info').html('小破站已经安全运行<span id="span_dt_dt" style="color: #fff;"></span>');
+$('.framework-info').html('小破站已经安全运�?span id="span_dt_dt" style="color: #fff;"></span>');
 window.setTimeout("show_date_time()", 1000);
 BirthDay=new Date("3/5/2022 0:0:0");
 today=new Date();
@@ -24,5 +24,5 @@ hrsold=Math.floor(e_hrsold);
 e_minsold=(e_hrsold-hrsold)*60;
 minsold=Math.floor((e_hrsold-hrsold)*60);
 seconds=Math.floor((e_minsold-minsold)*60);
-span_dt_dt.innerHTML='<font style=color:#afb4db>'+daysold+'</font> 天 <font style=color:#f391a9>'+hrsold+'</font> 时 <font style=color:#fdb933>'+minsold+'</font> 分 <font style=color:#a3cf62>'+seconds+'</font> 秒';
+span_dt_dt.innerHTML='<font style=color:#afb4db>'+daysold+'</font> �?<font style=color:#f391a9>'+hrsold+'</font> �?<font style=color:#fdb933>'+minsold+'</font> �?<font style=color:#a3cf62>'+seconds+'</font> �?;
 }
